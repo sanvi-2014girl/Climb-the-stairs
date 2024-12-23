078fa1f2ec962c43494014cde5a98ddd9cb8d236
sanvi-2014girl <goswamilipi82@gmail.com>
2024-12-23 11:28:41 +0530

    Climb the stairs

---
+++ main.py
@@ -0,0 +1,13 @@
+def ways(stairs):
+    if stairs<0:
+        return 0
+    if stairs==0:
+        return 1
+    twoSteps = 0
+    oneStep = 0
+    if (stairs>=2):
+        twoSteps = ways(stairs-2)
+    oneStep = ways(stairs-1)
+    return twoSteps+oneStep
+stairs = int(input("Enter number of steps: "))
+print(ways(stairs))
